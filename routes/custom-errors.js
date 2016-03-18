cf0f2b6ca1b5ef9141ee37f3c92220f54e60c548
Kristofor Carle <kristoforcarle@moabi.org>
2016-03-18 18:27:39 -0400

    fix error page content

--- routes/custom-errors.js
+++ routes/custom-errors.js
@@ -3,11 +3,35 @@
 module.exports = function(app) {
 
   app.get('/unauthorized', function (req, res) {
-    res.render('error', {code: 401, req});
+    var path = '';
+    if(req.query.path){
+      path = req.query.path;
+    }
+    res.status(401);
+    res.render('error', {
+      title: req.__('Unauthorized'),
+      props: {
+        title: req.__('Unauthorized'),
+        error: req.__('You are not authorized to access this page.'),
+        url: path
+      },
+       req});
   });
 
   app.get('/notfound', function (req, res) {
-    res.render('error', {code: 404, req});
+    var path = '';
+    if(req.query.path){
+      path = req.query.path;
+    }
+    res.status(404);
+    res.render('error',{
+    title: req.__('Page not found'),
+    props: {
+      title: req.__('Page not found'),
+      error: req.__('The page you requested was not found.'),
+      url: path
+    },
+     req});
   });
 
 };
