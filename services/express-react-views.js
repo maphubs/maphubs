4d7ab7f7cc68cd71638b845ae06ce20494ef552f
Kristofor Carle <kristoforcarle@moabi.org>
2016-03-22 13:55:57 -0400

    only load Google Analytics in production

--- services/express-react-views.js
+++ services/express-react-views.js
@@ -205,18 +205,23 @@ function createEngine(engineOptions) {
                   var g = d.createElement(t);g.id = 'doorbellScript';g.type = 'text/javascript';g.async = true;g.src = 'https://embed.doorbell.io/button/2854?t='+(new Date().getTime());(d.getElementsByTagName('head')[0]||d.getElementsByTagName('body')[0]).appendChild(g);
               }(document, 'script'));
           </script>
-          <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-            ga('create', 'UA-75234840-1', 'auto');
-            ga('send', 'pageview');
-
-          </script>
           `;
         }
+      if(process.env.NODE_ENV == 'production'){
+        markup += `
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-75234840-1', 'auto');
+          ga('send', 'pageview');
+
+        </script>
+        `;
+      }
       markup +=
       '</body>\n' +
       '</html>\n'
