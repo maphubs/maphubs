b6242d1acde1404fc7b6eab6e8cfaf04d397e74f
Kristofor Carle <kristoforcarle@moabi.org>
2016-05-31 13:25:48 -0400

    use localhost in client config example

--- clientconfig-example.js
+++ clientconfig-example.js
@@ -2,6 +2,6 @@ module.exports = {
   host: 'dev.localhost', //Maphubs needs to run from a subdomain if using localhost
   port: 4000,
   https: false,
-  tileServiceUrl: 'http://192.168.59.100:4001',
+  tileServiceUrl: 'http://localhost:4001',
   MAPBOX_ACCESS_TOKEN: "12345"
 };
