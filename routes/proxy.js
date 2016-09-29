72a3fa05fe550b5fb30de1be03f864fb498f1956
Kristofor Carle <kris@maphubs.com>
2016-09-29 17:32:55 -0400

    fix missing local config

--- routes/proxy.js
+++ routes/proxy.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
 
   //if tiles requests make it to the web app, proxy them from here
   //needed for generating screenshots on local MapHubs Pro deployments
-  app.use('/tiles', proxy(local.tileServiceInternal, {
+  app.use('/tiles', proxy(local.tileServiceInternalUrl, {
   forwardPath: function(req) {
     var path = '/tiles' + require('url').parse(req.url).path;
     debug(path);
