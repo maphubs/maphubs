87eb6f10ee025c3a09383e3e78d0622dd0506db1
Kristofor Carle <kris@maphubs.com>
2016-11-17 03:18:40 -0500

    fix select lists that are larger than screen height

--- src/services/express-react-views.js
+++ src/services/express-react-views.js
@@ -103,28 +103,32 @@ function createEngine(engineOptions) {
           `;
         }
 
+        var iconFolder = MAPHUBS_CONFIG.theme;
+        if(MAPHUBS_CONFIG.theme === 'maphubs-pro'){
+          iconFolder = 'maphubs';
+        }
         //icons
         markup += `
-        <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/assets/themes/`+ MAPHUBS_CONFIG.theme +`/apple-touch-icon-57x57.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/assets/themes/`+ MAPHUBS_CONFIG.theme +`/apple-touch-icon-114x114.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/assets/themes/`+ MAPHUBS_CONFIG.theme +`/apple-touch-icon-72x72.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/assets/themes/`+ MAPHUBS_CONFIG.theme +`/apple-touch-icon-144x144.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="60x60" href="/assets/themes/`+ MAPHUBS_CONFIG.theme +`/apple-touch-icon-60x60.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/assets/themes/`+ MAPHUBS_CONFIG.theme +`/apple-touch-icon-120x120.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="76x76" href="/assets/themes/`+ MAPHUBS_CONFIG.theme +`/apple-touch-icon-76x76.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/assets/themes/`+ MAPHUBS_CONFIG.theme +`/apple-touch-icon-152x152.png" />
-        <link rel="icon" type="image/png" href="/assets/themes/`+ MAPHUBS_CONFIG.theme +`/favicon-196x196.png" sizes="196x196" />
-        <link rel="icon" type="image/png" href="/assets/themes/`+ MAPHUBS_CONFIG.theme +`/favicon-96x96.png" sizes="96x96" />
-        <link rel="icon" type="image/png" href="/assets/themes/`+ MAPHUBS_CONFIG.theme +`/favicon-32x32.png" sizes="32x32" />
-        <link rel="icon" type="image/png" href="/assets/themes/`+ MAPHUBS_CONFIG.theme +`/favicon-16x16.png" sizes="16x16" />
-        <link rel="icon" type="image/png" href="/assets/themes/`+ MAPHUBS_CONFIG.theme +`/favicon-128.png" sizes="128x128" />
-        <meta name="application-name" content="` + MAPHUBS_CONFIG.productName +`"/>
+        <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/assets/themes/`+ iconFolder +`/apple-touch-icon-57x57.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/assets/themes/`+ iconFolder +`/apple-touch-icon-114x114.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/assets/themes/`+ iconFolder +`/apple-touch-icon-72x72.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/assets/themes/`+ iconFolder +`/apple-touch-icon-144x144.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="60x60" href="/assets/themes/`+ iconFolder +`/apple-touch-icon-60x60.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/assets/themes/`+ iconFolder +`/apple-touch-icon-120x120.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="76x76" href="/assets/themes/`+ iconFolder +`/apple-touch-icon-76x76.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/assets/themes/`+ iconFolder +`/apple-touch-icon-152x152.png" />
+        <link rel="icon" type="image/png" href="/assets/themes/`+ iconFolder +`/favicon-196x196.png" sizes="196x196" />
+        <link rel="icon" type="image/png" href="/assets/themes/`+ iconFolder +`/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/png" href="/assets/themes/`+ iconFolder +`/favicon-32x32.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="/assets/themes/`+ iconFolder +`/favicon-16x16.png" sizes="16x16" />
+        <link rel="icon" type="image/png" href="/assets/themes/`+ iconFolder +`/favicon-128.png" sizes="128x128" />
+        <meta name="application-name" content="` + iconFolder +`"/>
         <meta name="msapplication-TileColor" content="#FFFFFF" />
-        <meta name="msapplication-TileImage" content="/assets/themes/`+ MAPHUBS_CONFIG.theme +`/mstile-144x144.png" />
-        <meta name="msapplication-square70x70logo" content="/assets/themes/`+ MAPHUBS_CONFIG.theme +`/mstile-70x70.png" />
-        <meta name="msapplication-square150x150logo" content="/assets/themes/`+ MAPHUBS_CONFIG.theme +`/mstile-150x150.png" />
-        <meta name="msapplication-wide310x150logo" content="/assets/themes/`+ MAPHUBS_CONFIG.theme +`/mstile-310x150.png" />
-        <meta name="msapplication-square310x310logo" content="/assets/themes/`+ MAPHUBS_CONFIG.theme +`/mstile-310x310.png" />
+        <meta name="msapplication-TileImage" content="/assets/themes/`+ iconFolder +`/mstile-144x144.png" />
+        <meta name="msapplication-square70x70logo" content="/assets/themes/`+ iconFolder +`/mstile-70x70.png" />
+        <meta name="msapplication-square150x150logo" content="/assets/themes/`+ iconFolder +`/mstile-150x150.png" />
+        <meta name="msapplication-wide310x150logo" content="/assets/themes/`+ iconFolder +`/mstile-310x150.png" />
+        <meta name="msapplication-square310x310logo" content="/assets/themes/`+ iconFolder +`/mstile-310x310.png" />
         `;
 
         if(options.oembed){
