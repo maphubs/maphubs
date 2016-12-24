4f519ccda8dadd7fb8a42833e20c70871b31e574
Kristofor Carle <kris@maphubs.com>
2016-12-24 16:13:28 -0500

    fix GeoJSONSource deprecated in AGS query sources

--- src/components/Map/Sources/AGSFeatureServerQuery.js
+++ src/components/Map/Sources/AGSFeatureServerQuery.js
@@ -1,29 +1,18 @@
 var TerraformerGL = require('../../../services/terraformerGL.js');
-var mapboxgl = {};
-if (typeof window !== 'undefined') {
-    mapboxgl = require("../../../../assets/assets/js/mapbox-gl/mapbox-gl.js");
-}
 var debug = require('../../../services/debug')('AGSFeatureServerQuery');
 var AGSFeatureServerQuery = {
   load(key, source, map){
     var _this = this;
     return TerraformerGL.getArcGISFeatureServiceGeoJSON(source.url)
       .then(function(geoJSON) {
-
         if(geoJSON.bbox && geoJSON.bbox.length > 0 && this.state.allowLayersToMoveMap){
           _this.zoomToData(geoJSON);
         }
-
-        var geoJSONSource = new mapboxgl.GeoJSONSource({data: geoJSON});
-        map.addSource(key, geoJSONSource);
+        map.addSource(key, {"type": "geojson", data: geoJSON});
       }, function(error) {
        debug('(' + _this.state.id + ') ' +error);
       });
-  },
-  onStyleLoad(){
-
   }
-
 };
 
 module.exports = AGSFeatureServerQuery;
--- src/components/Map/Sources/AGSMapServerQuery.js
+++ src/components/Map/Sources/AGSMapServerQuery.js
@@ -1,29 +1,18 @@
 var TerraformerGL = require('../../../services/terraformerGL.js');
-var mapboxgl = {};
-if (typeof window !== 'undefined') {
-    mapboxgl = require("../../../../assets/assets/js/mapbox-gl/mapbox-gl.js");
-}
 var debug = require('../../../services/debug')('AGSFeatureServerQuery');
 var AGSMapServerQuery = {
   load(key, source, map){
     var _this = this;
     return TerraformerGL.getArcGISGeoJSON(source.url)
       .then(function(geoJSON) {
-
         if(geoJSON.bbox && geoJSON.bbox.length > 0 && _this.state.allowLayersToMoveMap){
           _this.zoomToData(geoJSON);
         }
-
-        var geoJSONSource = new mapboxgl.GeoJSONSource({data: geoJSON});
-        map.addSource(key, geoJSONSource);
+        map.addSource(key, {"type": "geojson", data: geoJSON});
       }, function(error) {
         debug('(' + _this.state.id + ') ' +error);
       });
-  },
-  onStyleLoad(){
-
   }
-
 };
 
 module.exports = AGSMapServerQuery;
