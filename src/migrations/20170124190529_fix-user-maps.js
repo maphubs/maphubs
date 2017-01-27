764e3bf4be02334bf115f7999d12065c8051a122
Kristofor Carle <kris@maphubs.com>
2017-01-27 14:39:00 -0500

    fix user map migrate script

--- src/migrations/20170124190529_fix-user-maps.js
+++ src/migrations/20170124190529_fix-user-maps.js
@@ -1,13 +1,10 @@
 
 exports.up = function(knex, Promise) {
-  return knex.raw(`ALTER TABLE omh.maps ADD COLUMN owned_by_user_id bigint;`)
-    .then(function(){
-      return  knex.raw(`
+  return knex.raw(`
         UPDATE omh.maps a SET owned_by_user_id = b.user_id
         FROM omh.user_maps AS b
         WHERE a.map_id = b.map_id;
       `);
-    });
 };
 
 exports.down = function(knex, Promise) {
