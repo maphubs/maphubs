a2abc97a2c7bbce327d6be0d179fda63d9e9da1a
Kristofor Carle <kris@maphubs.com>
2017-02-24 15:26:17 -0500

    add SVG export

--- src/routes/secure/exports.js
+++ src/routes/secure/exports.js
@@ -4,6 +4,8 @@ var ogr2ogr = require('ogr2ogr');
 var tokml = require('tokml');
 var debug = require('../../services/debug')('exports');
 var privateLayerCheck = require('../../services/private-layer-check').middleware;
+var knex = require('../../connection.js');
+var Promise = require('bluebird');
 
 module.exports = function(app) {
 
@@ -11,6 +13,58 @@ module.exports = function(app) {
       var layer_id = parseInt(req.params.layer_id || '', 10);
       Layer.getGeoJSON(layer_id).then(function(geoJSON){
         res.status(200).send(geoJSON);
+      }).catch(apiError(res, 200));
+  });
+
+
+  app.get('/api/layer/:layer_id/export/svg/*', privateLayerCheck, function(req, res) {
+      var layer_id = parseInt(req.params.layer_id || '', 10);
+      Layer.getLayerByID(layer_id).then(function(layer){
+        return Promise.all([
+          knex.raw(`select ST_AsSVG(ST_Transform(wkb_geometry, 900913)) as svg from layers.data_${layer.layer_id};`),
+          knex.raw(`select ST_XMin(bbox)::float as xmin, 
+            ST_YMin(bbox)::float as ymin, 
+            ST_XMax(bbox)::float as xmax, ST_YMax(bbox)::float as ymax 
+            from (select ST_Extent(ST_Transform(wkb_geometry, 900913)) as bbox from layers.data_${layer.layer_id}) a`)
+        ]).then(function(results){
+          var featureSVGs = results[0];
+          var bounds = results[1].rows[0];
+          var paths = '';
+          var color = '#FF0000';
+          if(layer.settings &&  layer.settings.color){
+            color = layer.settings.color;
+          } 
+          
+          if(layer.data_type === 'point'){
+            featureSVGs.rows.forEach(function(row){
+               paths += `<path d="${row.svg}"></path>`;
+            });
+
+          }else if(layer.data_type === 'line'){
+            featureSVGs.rows.forEach(function(row){
+               paths += `<path d="${row.svg}"></path>`;
+            });
+          }else if(layer.data_type === 'polygon'){
+            featureSVGs.rows.forEach(function(row){
+               paths += `<path fill="${color}" stroke="black" stroke-width="3000" d="${row.svg}"></path>`;
+            });
+
+           
+          }
+
+          var width = bounds.xmax-bounds.xmin;
+          var height = bounds.ymax-bounds.ymin;
+
+          var svg = `
+          <svg xmlns="http://www.w3.org/2000/svg"
+          id="maphubs-layer-${layer.layer_id}" viewBox="${bounds.xmin} ${bounds.ymin} ${width} ${height*2}" preserveAspectRatio="xMidYMid meet">
+          ${paths}
+          </svg>
+          `;
+
+          res.header("Content-Type", "image/svg+xml");
+          res.status(200).send(svg);
+        });
       }).catch(apiError(res, 200));
   });
 
