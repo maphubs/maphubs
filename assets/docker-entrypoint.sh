79d0ce01bfb26128a0dadef624063f0d9a23826f
Kristofor Carle <kris@maphubs.com>
2017-03-12 15:51:11 -0400

    remove iD editor from assets

--- assets/docker-entrypoint.sh
+++ assets/docker-entrypoint.sh
@@ -1,13 +1,2 @@
 #!/bin/sh
-
-#write iD config
-cat <<EOF >/data/edit/js/config.js
-var OMH_CONFIG = {
-  AUTH_URL: "${ID_AUTH_URL}",
-  OAUTH_CONSUMER_KEY: "${ID_KEY}",
-  OAUTH_SECRET: "${ID_SECRET}"
-};
-EOF
-
-#start nginx
 nginx -g 'daemon off;'
