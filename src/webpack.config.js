b0e5828d02f45b9fb278084fbd2ae11c2ca50176
Kristofor Carle <kris@maphubs.com>
2017-05-04 19:03:24 -0400

    fix webpack warnings

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -96,7 +96,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         loader: 'babel-loader',
-        include: [/i18n\.js/, /locales/, /views/, /components/, /stores/, /actions/, /services/, /client/, /medium-editor/, /react-data-grid/, /react-disqus-thread/, /react-colorpickr/, /mapbox-gl/],       
+        include: [/i18n\.js/, /locales/, /views/, /components/, /stores/, /actions/, /services/, /client/, /medium-editor/, /react-disqus-thread/, /react-colorpickr/, /mapbox-gl/],       
         options: {
           presets: [
             "es2015",
