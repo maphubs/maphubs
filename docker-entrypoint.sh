9ce55f1b89f00cee853737a4631ebb2e78fed3b5
Kristofor Carle <kris@maphubs.com>
2017-05-17 14:49:47 -0400

    remove unused env vars

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -30,9 +30,7 @@ var MAPHUBS_CONFIG = {
   homepageSlides: ${OMH_HOMEPAGE_SLIDES},
   homepageMapHubId: "${OMH_HOMEPAGE_MAP_HUB_ID}",
   homepageMailingList: ${OMH_HOMEPAGE_MAILINGLIST},
-  mapHubsProDemo: ${OMH_MAPHUBS_PRO_DEMO},
-  copyrightText: "${OMH_COPYRIGHT_TEXT}",
-  useMapboxBaseMaps: ${OMH_USE_MAPBOX_BASEMAPS}
+  mapHubsProDemo: ${OMH_MAPHUBS_PRO_DEMO}
 };
 if(typeof module !== 'undefined'){
   module.exports = MAPHUBS_CONFIG;
