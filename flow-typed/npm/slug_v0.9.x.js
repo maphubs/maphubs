c6c02ffdbcb2d7c156f775c596723bd1126a8c94
Kristofor Carle <kris@maphubs.com>
2017-06-10 23:04:11 -0400

    update flow-typed

--- flow-typed/npm/slug_v0.9.x.js
+++ flow-typed/npm/slug_v0.9.x.js
@@ -1,17 +1,17 @@
-// flow-typed signature: 5d1365421f3793af8b0e98a1995f79b4
-// flow-typed version: 7154c86e07/slug_v0.9.x/flow_>=v0.28.x
+// flow-typed signature: aa2cc901101dc881d88fa9b82d248a4e
+// flow-typed version: dbef68335c/slug_v0.9.x/flow_>=v0.28.x
 
 type SlugMode = 'rfc3986' | 'pretty'
 
 declare module 'slug' {
   declare type SlugOptions = {
-    mode: SlugMode,
-    replacement: string,
-    multicharmap: { [key: string]: string },
-    charmap: { [key: string]: string },
-    remove: RegExp,
-    lower: boolean,
-    symbol: boolean,
+    mode?: SlugMode,
+    replacement?: string,
+    multicharmap?: { [key: string]: string },
+    charmap?: { [key: string]: string },
+    remove?: ?RegExp,
+    lower?: boolean,
+    symbols?: boolean,
   }
   declare module.exports: {
       (input: string, optionOrReplacement?: string | SlugOptions): string,
