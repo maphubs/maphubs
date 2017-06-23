8bfb3ca8a9a93eba88891300d7fd9373273ef41c
Kristofor Carle <kris@maphubs.com>
2017-06-23 13:59:20 -0400

    fix typo in forget password link

--- src/views/auth0login.js
+++ src/views/auth0login.js
@@ -67,7 +67,7 @@ export default class Login extends MapHubsComponent<DefaultProps, Props, State> 
       <div>
         <div className="page-footer no-margin no-padding valign-wrapper" style={{height: '10%'}}>
             <div className="row center valign" style={{fontSize: '16px', paddingLeft: '10%', paddingRight: '10%'}}>
-              {this.__('We recently upgraded our login system. If you are unable to access your account please try to')} <a href="forgetpassword">{this.__('reset your password')}</a> {this.__('or contact us at')} <a href="mailto:support@maphubs.com">support@maphubs.com</a> 
+              {this.__('We recently upgraded our login system. If you are unable to access your account please try to')} <a href="forgotpassword">{this.__('reset your password')}</a> {this.__('or contact us at')} <a href="mailto:support@maphubs.com">support@maphubs.com</a> 
             </div>
          
           </div>
