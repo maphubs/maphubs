a8a17279bb272aa3c97fcc213864bfc3ef3dbf8f
Kristofor Carle <kris@maphubs.com>
2017-06-25 19:19:29 -0400

    fix bad css path

--- src/client/createhubstory.js
+++ src/client/createhubstory.js
@@ -8,7 +8,7 @@ require("materialize-css");
 require('../../node_modules/react-colorpickr/dist/colorpickr.css');
 
 require('./story.css');
-require('mapbox-gl/dist/mapbox-gl.css');
+require('../../node_modules/mapbox-gl/dist/mapbox-gl.css');
 require('@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css');
 require('medium-editor/dist/css/medium-editor.css');
 require('medium-editor/dist/css/themes/flat.css');
