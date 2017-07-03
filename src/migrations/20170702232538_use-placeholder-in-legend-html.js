0a0eef17243bb25f7dc7dc7e05da2b34e5472ea4
Kristofor Carle <kris@maphubs.com>
2017-07-03 17:16:05 -0400

    use placeholder in legends, fixes #210

---
+++ src/migrations/20170702232538_use-placeholder-in-legend-html.js
@@ -0,0 +1,9 @@
+
+exports.up = function(knex) {
+  return knex.raw(`UPDATE omh.layers 
+SET legend_html = regexp_replace(legend_html, '<h3>.*?<\\/h3>' , '<h3>{NAME}</h3>')`);
+};
+
+exports.down = function() {
+  
+};
