484f7c6782f00dbeb98ac85f6ee21cf8d408faa5
Kristofor Carle <kris@maphubs.com>
2017-07-08 15:06:02 -0400

    add quick 404 responses

---
+++ src/routes/public-routes/quick-404.js
@@ -0,0 +1,14 @@
+module.exports = function(app: any) {
+
+  //don't bother even rendering the error page for these...
+  const quick404ErrorPages = [
+    '/wp-login.php', '/xmlrpc.php'
+  ];
+
+  quick404ErrorPages.map(page => {
+    app.get(page,  (req, res) => {
+      res.status(404).send();
+    });
+  });
+
+};
