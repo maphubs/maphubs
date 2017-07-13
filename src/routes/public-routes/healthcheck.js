906bf230e3919ac0ee767e8a65f7cca8a26b31c4
Kristofor Carle <kris@maphubs.com>
2017-07-13 17:44:42 -0400

    fix healthcheck query

--- src/routes/public-routes/healthcheck.js
+++ src/routes/public-routes/healthcheck.js
@@ -5,8 +5,17 @@ module.exports = function(app: any) {
 
   app.get('/healthcheck',  (req, res) => {
     if(knex){
-      knex.select(knex.raw('SELECT 1')).then(()=>{
-       return res.status(200).send('OK');
+      knex.select(knex.raw('version()')).then((result)=>{
+        if(result && 
+          Array.isArray(result) && 
+          result.length === 1 &&
+          result[0].version
+        ){
+          return res.status(200).send('OK');
+        }else{
+         return res.status(500).send();
+        }
+       
       })
       .catch((err) => {
         log.error(err.message);
