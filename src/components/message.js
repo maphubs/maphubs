527e459dcb09543340077bfc3a0787cf08f19750
Kristofor Carle <kris@maphubs.com>
2017-08-02 02:11:08 -0400

    support lists of errors in error message (for listing bad data on import)

--- src/components/message.js
+++ src/components/message.js
@@ -35,11 +35,58 @@ export default class Message extends MapHubsComponent<void, Props, MessageStoreS
 
   render(){
     /*eslint-disable react/no-danger*/
+
+    let messageDisplay = '';
+    let message;
+    if(this.state.message){
+      message = JSON.parse(this.state.message);
+    }
+    if(typeof message === 'string'){
+      messageDisplay = (
+         <div dangerouslySetInnerHTML={{__html:message}}></div>
+      );
+    }else if(Array.isArray(message) && message.length > 0){
+      messageDisplay = (
+        <table>
+          <tbody>
+          {
+            message.map((messageItem, i)=>{
+              if(typeof messageItem === 'string'){
+                return (
+                  <tr><td>{messageItem}</td></tr>
+                );
+              }else if(typeof messageItem === 'object'){
+                return (
+                  <tr key={i}>{
+                    Object.keys(messageItem).map(key=>{
+                      let val = JSON.stringify(messageItem[key]);
+                      return (
+                        <td key={key}><b>{key}:</b>{val}</td>
+                      );
+                    })
+                  }</tr>
+                );
+              }else{
+                return (
+                  <tr>{messageItem.toString()}</tr>
+                );
+              }
+            })
+          }
+          </tbody>
+        </table>
+      );
+    }else{
+      messageDisplay = (
+        <div dangerouslySetInnerHTML={{__html: this.state.message.toString()}}></div>
+      );
+    }
+
     return (
       <Modal id="message-modal" show={this.state.show} fixedFooter={false}>
         <ModalContent>
           <h4>{this.state.title}</h4>
-          <div dangerouslySetInnerHTML={{__html: this.state.message.toString()}}></div>
+          {messageDisplay}
         </ModalContent>
         <ModalFooter>
           <a href="#!" className=" modal-action modal-close waves-effect waves-light btn-flat" onClick={this.onDismiss}>{this.__('Okay')}</a>
