5c8ef0add69085e0126b82f9e137de0f47630b99
Kristofor Carle <kris@maphubs.com>
2017-08-15 20:13:09 -0400

    fix planet layer style update

--- src/migrations/20170815141416_update-planet-layer-styles.js
+++ src/migrations/20170815141416_update-planet-layer-styles.js
@@ -1,28 +1,5 @@
 global.MAPHUBS_CONFIG = require('../clientconfig');
 
-var updatePlanetConfig = function(elc){
-  if(elc.layers){
-    //new multi-layer config
-    elc.layers = elc.layers.map(layer => {
-      let sceneArr = layer.planet_labs_scene.split(':');
-      let type = sceneArr[0].trim();
-      let scene = sceneArr[1].trim();
-      var url = `https://tiles.planet.com/data/v1/${type}/${scene}/{z}/{x}/{y}.png?api_key=${MAPHUBS_CONFIG.PLANET_LABS_API_KEY}`;
-      layer.tiles = [url];
-      return layer;
-    });
-  }else {
-    let scene = elc.planet_labs_scene;
-    var url = `https://tiles.planet.com/data/v1/PSOrthoTile/${scene}/{z}/{x}/{y}.png?api_key=${MAPHUBS_CONFIG.PLANET_LABS_API_KEY}`;
-    elc.tiles =[url];
-    elc.layers = [{
-      planet_labs_scene: elc.scene,
-      tiles: elc.tiles
-    }];
-  }
-  return elc;
-};
-
 var multiRasterStyleWithOpacity = function(layer_id, layers, opacity){
   
       opacity = opacity / 100;
@@ -52,10 +29,8 @@ var multiRasterStyleWithOpacity = function(layer_id, layers, opacity){
 
       };
       });
-
       return style;
     };
-
 
 exports.up = function(knex, Promise) {
   return knex('omh.layers').select('layer_id', 'external_layer_config', 'style')
@@ -63,9 +38,8 @@ exports.up = function(knex, Promise) {
   .then((layers) => {
     var updateCommands = [];
     layers.forEach(layer => {
-      let elc = updatePlanetConfig(layer.external_layer_config);
-      let style = multiRasterStyleWithOpacity(layer.layer_id, elc.layers);
-      updateCommands.push(knex('omh.layers').update({external_layer_config: elc, style}).where({layer_id: layer.layer_id}));
+      let style = multiRasterStyleWithOpacity(layer.layer_id, layer.external_layer_config, 100);
+      updateCommands.push(knex('omh.layers').update({style}).where({layer_id: layer.layer_id}));
     });
     return Promise.all(updateCommands);
   });
