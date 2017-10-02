f130c3f85b73794358a8a49eb5ea8e2672b37b4b
Kristofor Carle <kris@maphubs.com>
2017-10-01 23:23:44 -0400

    add .maphubs file exports to robots.txt

--- src/routes/secure/sitemap.js
+++ src/routes/secure/sitemap.js
@@ -21,7 +21,17 @@ module.exports = function(app) {
       res.send('User-agent: *\nDisallow: /');
     }else{
       //don't crawl exports
-      res.send('User-agent: *\nDisallow: /*.kml$\nDisallow: /*.zip$\nDisallow: /*.geojson$\nDisallow: /*.gpx$\nDisallow: /*.csv$\nDisallow: /*.svg$\nDisallow: /*.pbf$\nDisallow: /xml/map/*');
+      res.send(`User-agent: *
+Disallow: /*.kml$
+Disallow: /*.maphubs$
+Disallow: /*.zip$
+Disallow: /*.geojson$
+Disallow: /*.gpx$
+Disallow: /*.csv$
+Disallow: /*.svg$
+Disallow: /*.pbf$
+Disallow: /xml/map/*
+`);
     }
   });
 
