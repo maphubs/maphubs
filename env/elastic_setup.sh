8456f7e4b80497603b170977ab7c6a7692c78aa4
Kristofor Carle <kris@maphubs.com>
2017-10-31 17:39:49 -0400

    update elasticsearch setup script

--- env/elastic_setup.sh
+++ env/elastic_setup.sh
@@ -5,5 +5,4 @@ curl -XPUT -u elastic 'localhost:9200/_xpack/security/user/elastic/_password' -d
 curl -XPUT -u elastic 'localhost:9200/_xpack/security/user/kibana/_password' -d '{
   "password" : ""
 }'
-
-curl -XPUT -u elastic 'localhost:9200/_xpack/license' -H "Content-Type: application/json" -d @license.json
+curl -XPUT -u elastic 'http://<host>:<port>/_xpack/license?acknowledge=true' -H "Content-Type: application/json" -d @license.json
