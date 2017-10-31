7b38b9408772a8df2ba9e29b0eff4e3e506b358e
Kristofor Carle <kris@maphubs.com>
2017-10-30 21:30:23 -0400

    upgrade postgres container; add cluster support

---
+++ db-server/remove-old-osm-tables.sql
@@ -0,0 +1,28 @@
+drop table if exists current_relations cascade;
+drop table if exists current_ways cascade;
+drop table if exists current_nodes cascade;
+drop table if exists current_node_tags cascade;
+drop table if exists node_tags cascade;
+drop table if exists nodes cascade;
+drop table if exists relations cascade;
+drop table if exists relation_members cascade;
+drop table if exists relation_tags cascade;
+drop table if exists current_relation_tags cascade;
+drop table if exists current_relation_members cascade;
+drop table if exists current_way_tags cascade;
+drop table if exists current_way_nodes cascade;
+drop table if exists ways cascade;
+drop table if exists way_tags cascade;
+drop table if exists way_nodes cascade;
+drop table if exists gpx_file_tags cascade;
+drop table if exists gpx_files cascade;
+drop table if exists gps_points cascade;
+
+drop table if exists diary_comments cascade;
+drop table if exists diary_entries cascade;
+drop table if exists friends cascade;
+drop table if exists languages cascade;
+drop table if exists messages cascade;
+drop table if exists changeset_tags cascade;
+drop table if exists changesets cascade;
+
