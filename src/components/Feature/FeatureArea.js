b733f81dc1ad7e6aa9139b4eb6c9f7228325a537
Kristofor Carle <kris@maphubs.com>
2018-02-04 16:45:34 -0500

    add feature area comparison

--- src/components/Feature/FeatureArea.js
+++ src/components/Feature/FeatureArea.js
@@ -18,6 +18,94 @@ type Props = {
   geojson?: Object
 }
 
+const comparisons = [
+  {
+    name: {
+      en: 'Grand Canyons'
+    },
+    ha: 313994.62
+  }, {
+    name: {
+      en: 'Rhode Islands'
+    },
+    ha: 313994.62
+  }, {
+    name: {
+      en: 'Luxembourgs'
+    },
+    ha: 258827.87
+  }, {
+    name: {
+      en: 'Washington, DCs'
+    },
+    ha: 17699.98
+  }, {
+    name: {
+      en: 'Manhattans'
+    },
+    ha: 5910.35
+  }, {
+    name: {
+      en: 'Central Parks'
+    },
+    ha: 341.10
+  }, {
+    name: {
+      en: 'Louvre Museums'
+    },
+    ha: 6.06
+  }, {
+    name: {
+      en: 'Football (soccer) Pitches'
+    },
+    ha: 0.71
+  }, {
+    name: {
+      en: 'Tennis Courts'
+    },
+    ha: 0.02
+  }
+]
+
+const findComparision = (areaHa: number) => {
+  let result
+  if (areaHa > comparisons[0].ha) {
+    result = {
+      name: comparisons[0].name,
+      val: areaHa / comparisons[0].ha
+    }
+  } else {
+    comparisons.forEach((comparison, i) => {
+      if (!result && areaHa > comparison.ha) {
+        if (i === 0) {
+          result = {
+            name: comparisons[0].name,
+            val: areaHa / comparisons[0].ha
+          }
+        } else if (i === comparisons.length - 1) {
+          result = {
+            name: comparisons[i - 1].name,
+            val: areaHa / comparisons[i - 1].ha
+          }
+        } else {
+          result = {
+            name: comparison.name,
+            val: areaHa / comparison.ha
+          }
+        }
+      }
+    })
+    if (!result) {
+      const last = comparisons.length - 1
+      result = {
+        name: comparisons[last].name,
+        val: areaHa / comparisons[last].ha
+      }
+    }
+  }
+  return result
+}
+
 export default class FeatureArea extends MapHubsPureComponent<Props, void> {
   render () {
     let featureAreaM2, featureAreaKM2, featureAreaHA
@@ -37,20 +125,32 @@ export default class FeatureArea extends MapHubsPureComponent<Props, void> {
         value = featureAreaKM2
         units = 'km²'
       }
+      const comparison = findComparision(featureAreaHA)
+
       return (
-        <div className='row'>
-          <h5>{this.__('Area')}</h5>
-          <span>
-            <IntlProvider locale={this.state.locale}>
-              <FormattedNumber value={value} />
-            </IntlProvider>&nbsp;{units}
-          </span>
-          <br />
-          <span>
-            <IntlProvider locale={this.state.locale}>
-              <FormattedNumber value={featureAreaHA} />
-            </IntlProvider>&nbsp;ha
-          </span>
+        <div>
+          <div className='row no-margin'>
+            <h5>{this.__('Area')}</h5>
+            <span>
+              <IntlProvider locale={this.state.locale}>
+                <FormattedNumber value={value} />
+              </IntlProvider>&nbsp;{units}
+            </span>
+            <br />
+            <span>
+              <IntlProvider locale={this.state.locale}>
+                <FormattedNumber value={featureAreaHA} />
+              </IntlProvider>&nbsp;ha
+            </span>
+          </div>
+          <div className='row no-margin'>
+            <span>
+              (or &nbsp;
+              <IntlProvider locale={this.state.locale}>
+                <FormattedNumber value={comparison.val} />
+              </IntlProvider>&nbsp;{this._o_(comparison.name)})
+            </span>
+          </div>
         </div>
       )
     }
