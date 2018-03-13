392684f44d4a5857537ae404348e25464a670ef5
Kristofor Carle <kris@maphubs.com>
2018-03-13 13:46:55 -0400

    fix checkboxes on import source selection

--- src/components/CreateLayer/SourceSelectionBox.js
+++ src/components/CreateLayer/SourceSelectionBox.js
@@ -32,8 +32,10 @@ export default class SourceSelectionBox extends MapHubsPureComponent<Props, void
         <form action='#' style={{height: '100%', position: 'relative'}} >
           {icon}
           <p className='no-margin' style={{position: 'absolute', bottom: '0'}}>
-            <input type='checkbox' className='filled-in' id={this.props.name + '-checkbox'} onChange={this.onSelect} checked={this.props.selected ? 'checked' : null} />
-            <label className='omh-accent-text' style={{fontSize: '13px'}} htmlFor={this.props.name + '-checkbox'}>{this.props.name}</label>
+            <label>
+              <input type='checkbox' className='filled-in' id={this.props.name + '-checkbox'} onChange={this.onSelect} checked={this.props.selected ? 'checked' : null} />
+              <span className='omh-accent-text' style={{fontSize: '13px'}}>{this.props.name}</span>
+            </label>
           </p>
         </form>
 
