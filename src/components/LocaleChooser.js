11d9e33762659c1f8f4e0fa66cfba6583e4fbf39
Kristofor Carle <kris@maphubs.com>
2018-04-23 19:04:55 -0400

    prevent locale chooser from crashing hub pages

--- src/components/LocaleChooser.js
+++ src/components/LocaleChooser.js
@@ -19,15 +19,19 @@ export default class LocaleChooser extends MapHubsComponent<Props, State> {
   }
 
   componentDidMount () {
-    M.Dropdown.init(this.refs.dropdown, {
-      inDuration: 300,
-      outDuration: 225,
-      constrainWidth: false, // Does not change width of dropdown to that of the activator
-      hover: false, // Activate on hover
-      gutter: 0, // Spacing from edge
-      coverTrigger: false, // Displays dropdown below the button
-      alignment: 'right' // Displays dropdown with edge aligned to the left of button
-    })
+    try {
+      M.Dropdown.init(this.dropdown, {
+        inDuration: 300,
+        outDuration: 225,
+        constrainWidth: false, // Does not change width of dropdown to that of the activator
+        hover: false, // Activate on hover
+        gutter: 0, // Spacing from edge
+        coverTrigger: false, // Displays dropdown below the button
+        alignment: 'right' // Displays dropdown with edge aligned to the left of button
+      })
+    } catch (err) {
+      debug.error(err)
+    }
   }
 
   shouldComponentUpdate (nextProps: Props, nextState: State) {
@@ -37,14 +41,12 @@ export default class LocaleChooser extends MapHubsComponent<Props, State> {
     return false
   }
 
-  onChange = (locale: string) => {
-    debug.log('LOCALE CHANGE: ' + locale)
-    LocaleActions.changeLocale(locale)
+  onChange = (e: Object) => {
+    debug.log('LOCALE CHANGE: ' + e.target.id)
+    LocaleActions.changeLocale(e.target.id)
   }
 
   render () {
-    const _this = this
-
     const options = {
       'en': {label: 'EN'},
       'fr': {label: 'FR'},
@@ -56,14 +58,20 @@ export default class LocaleChooser extends MapHubsComponent<Props, State> {
 
     return (
       <li className='nav-link-wrapper nav-dropdown-link-wrapper'>
-        <a ref='dropdown' className='locale-dropdown-button nav-dropdown-button dropdown-trigger'
-          href='#!' data-target={this.props.id} style={{paddingRight: 0}}>{label}
-          <i className='material-icons right' style={{marginLeft: 0}}>arrow_drop_down</i></a>
-        <ul id={this.props.id} className='dropdown-content'>
-          <li><a href='#!' onClick={function () { _this.onChange('en') }} className='nav-hover-menu-item'>English (EN)</a></li>
-          <li><a href='#!' onClick={function () { _this.onChange('fr') }} className='nav-hover-menu-item'>Français (FR)</a></li>
-          <li><a href='#!' onClick={function () { _this.onChange('es') }} className='nav-hover-menu-item'>Español (ES)</a></li>
-          <li><a href='#!' onClick={function () { _this.onChange('it') }} className='nav-hover-menu-item'>Italiano (IT)</a></li>
+        <a
+          ref={(el) => { this.dropdown = el }}
+          id='local-chooser-trigger'
+          className='locale-dropdown-button nav-dropdown-button dropdown-trigger'
+          href='#!'
+          data-target='local-chooser'
+          style={{paddingRight: 0}}>{label}
+          <i className='material-icons right' style={{marginLeft: 0}}>arrow_drop_down</i>
+        </a>
+        <ul id='local-chooser' className='dropdown-content'>
+          <li><a href='#!' id='en' onClick={this.onChange} className='nav-hover-menu-item'>English (EN)</a></li>
+          <li><a href='#!' id='fr' onClick={this.onChange} className='nav-hover-menu-item'>Français (FR)</a></li>
+          <li><a href='#!' id='es' onClick={this.onChange} className='nav-hover-menu-item'>Español (ES)</a></li>
+          <li><a href='#!' id='it' onClick={this.onChange} className='nav-hover-menu-item'>Italiano (IT)</a></li>
         </ul>
       </li>
     )
