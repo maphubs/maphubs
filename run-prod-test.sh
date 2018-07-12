70d3db95800dc3295e9a8453b2481f3890eff762
Kris Carle <kris@maphubs.com>
2018-07-11 20:16:44 -0400

    fix running local prod build

--- run-prod-test.sh
+++ run-prod-test.sh
@@ -2,4 +2,4 @@
 
 ./load-dev-env.sh
 
-DEBUG="maphubs:*,maphubs-error:*" NODE_ENV=production OMH_USE_LOCAL_ASSETS=true node --max-old-space-size=2048 server.js
+DEBUG="maphubs:*,maphubs-error:*" NODE_ENV=production ASSET_CDN_PREFIX= OMH_USE_LOCAL_ASSETS=true node --max-old-space-size=2048 server.js
