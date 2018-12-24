d21ad1c1592e5078e89fdbc847def0c8b7954c2e
Kristofor Carle <kris@maphubs.com>
2018-12-24 11:42:16 -0500

    additional logging on shapefile import

--- src/services/importers/shapefile/index.js
+++ src/services/importers/shapefile/index.js
@@ -18,9 +18,12 @@ const streamCloseToPromise = function (stream) {
 module.exports = async function (filePath: string, layer_id: number, config?: Object) {
   /* eslint-disable security/detect-non-literal-fs-filename */
   // file path is a folder from a env var + a GUID, not orginal filename
+  log.info('Starting Shapefile Importer')
   const pipedStream = fs.createReadStream(filePath).pipe(unzip.Extract({path: filePath + '_zip'}))
   await streamCloseToPromise(pipedStream)
+  log.info('Zip file extracted')
   // validate
+  log.info('Validating Shapefile')
   const result = await shapefileFairy(filePath, {extract: false})
   debug.log('ShapefileFairy Result: ' + JSON.stringify(result))
   if (result && result.code === 'MULTIPLESHP') {
