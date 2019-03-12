a475e71463fd480040c391948780a55077de6dd4
Kristofor Carle <kris@maphubs.com>
2019-03-12 19:43:26 -0400

    support custom colors for map category menu

--- src/components/Map/MapLayerMenu.js
+++ src/components/Map/MapLayerMenu.js
@@ -7,20 +7,24 @@ import LayerListDropDown from './LayerListDropDown'
 type Props = {
   categories: Array<Object>,
   layers: Array<Object>,
+  backgroundColor?: string,
+  textColor?: string,
   toggleVisibility: Function,
   t: Function
 }
 
 export default class MapLayerMenu extends React.PureComponent<Props, void> {
   render () {
-    const {categories, layers, toggleVisibility, t} = this.props
+    const {categories, layers, toggleVisibility, backgroundColor, textColor, t} = this.props
     return (
       <Row type='flex' justify='left'
         style={{
           boxShadow: '0 0 1px rgba(0,0,0,0.7)',
           borderTop: '1px #444 solid',
           height: '35px',
-          padding: '0px 15px'
+          padding: '0px 15px',
+          backgroundColor: backgroundColor || 'inherit',
+          color: textColor || 'inherit'
         }}>
         {categories.map((category, i) => {
           const categoriesLayers = []
