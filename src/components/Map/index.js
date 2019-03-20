a28e52d701cf4ffd5800062b74cd562d84e82da1
Kristofor Carle <kris@maphubs.com>
2019-03-20 00:03:06 -0400

    expose containers in Map SDK so components above can interact with map state

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -2,12 +2,18 @@ import Map from './Map'
 import InteractiveMap from './InteractiveMap'
 import Styles from './Styles'
 import Sources from './Sources'
+import MapContainer from './containers/MapContainer'
+import BaseMapContainer from './containers/BaseMapContainer'
+import DataEditorContainer from './containers/DataEditorContainer'
 
 export {
   Map,
   InteractiveMap,
   Styles,
-  Sources
+  Sources,
+  MapContainer,
+  BaseMapContainer,
+  DataEditorContainer
 }
 
 export default Map
