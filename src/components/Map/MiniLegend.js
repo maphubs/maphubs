f17ef98d833469f7c4aefaf8f372fc9c2c9461eb
Kristofor Carle <kris@maphubs.com>
2019-05-14 21:48:12 -0400

    upgrade nextjs and antd

--- src/components/Map/MiniLegend.js
+++ src/components/Map/MiniLegend.js
@@ -220,9 +220,7 @@ export default class MiniLegend extends React.Component<Props, State> {
 
           .word-wrap {
             overflow-wrap: break-word;
-            word-wrap: break-word;
             -ms-word-break: break-all;
-            word-break: break-word;
             -ms-hyphens: auto;
             -moz-hyphens: auto;
             -webkit-hyphens: auto;
