f57d13f646f66e63fa350b062792e9baa69d1e45
Kristofor Carle <kris@maphubs.com>
2021-09-01 18:39:02 -0400

    fix run-knex using old env files

--- run-knex.sh
+++ run-knex.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-export $(cat env/dev/db.env | grep -v ^# | xargs) && \
-export $(cat env/dev/web.env | grep -v ^# | xargs) && \
-export $(cat env/dev/host.env | grep -v ^# | xargs) && \
-export $(cat env/dev/secret/db-pass.env | grep -v ^# | xargs) && \
-export $(cat env/dev/secret/web-secret.env | grep -v ^# | xargs) && \
+export $(cat .env.local | grep -v ^# | xargs) && \
+
 node ./node_modules/knex/bin/cli.js --knexfile=./src/knexfile.js $@
